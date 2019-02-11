9ffc1ce817fe71392a04ffc6db274ea84539f87e
kavi0310 <46838903+kavi0310@users.noreply.github.com>
2019-02-12 01:22:05 +0530

    Update 36.c

--- 36.c
+++ 36.c
@@ -2,16 +2,16 @@
 
 void main()
 {
-    int h1,h2,m1,m2,h,m;
+    int h1,h2,m1,m2,a,b;
     scanf("%d%d%d%d",&h1,&m1,&h2,&m2);
     if(h1>h2)
-    h=h1-h2;
+    a=h1-h2;
     else
-    h=h2-h1;
+    a=h2-h1;
     if(m1>m2)
-    m=m1-m2;
+    b=m1-m2;
     else
-    m=m2-m1;
-    printf("%d %d",h,m);
+    b=m2-m1;
+    printf("%d %d",a,b);
     getch();
 }
